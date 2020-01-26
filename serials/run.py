ee816f8c42b6eeeb82bd038835aea511ba757b05
Vendetta <tarthewongkham0@gmail.com>
2020-01-26 16:32:43 +0700

    refactor UI initiation

--- serials/run.py
+++ serials/run.py
@@ -7,6 +7,17 @@ from tkinter.ttk import Frame, Style
 from image_util import read_icons
 
 
+def get_datetime(sep="\n"):
+    return datetime.now().strftime(f"%d/%m/%Y{sep}%H:%M:%S")
+
+
+def to_time_format(counter):
+    hour, remaining = divmod(counter, 3600)
+    minutes, seconds = divmod(remaining, 60)
+    _elapse_time = "{:02d}:{:02d}".format(int(minutes), int(seconds))
+    return _elapse_time
+
+
 class Example(Frame):
 
     def __init__(self, root, width, height):
@@ -14,12 +25,12 @@ class Example(Frame):
 
         self.pedal = 0
         self.hall = 0
-        self.temp = 1
-        self.batt_percentage = 100
+        self.temp = 0
         self.speed = 0
         self.power = 0
         self.count = 0
         self.distance = 0
+        self.batt_percentage = 29
 
         self.VU = read_icons('images/vu20/*.png', 500, 100)
         self.BATT = read_icons('images/battery/*.png', 150, 100)
@@ -37,35 +48,54 @@ class Example(Frame):
         self.update_time()
 
     def press(self, event):
-        d = 1
-        if self.pedal < 100:
-            self.pedal += d
-            self.pedal = min(100, self.pedal)
-        for i in range(d):
-            val, rem = divmod(self.pedal, 10)
-            img = self.VU[str((val * 10) + (rem // 5 * 5))]
-            self.vu_lbl.configure(image=img)
-            self.vu_lbl.image = img
-            self.pad_lbl.configure(text='{:>4}%'.format(self.pedal))
+        self.pedal += 1
+        self.pedal = min(100, max(0, self.pedal))
+        self.update_vu(self.pedal)
 
     def release(self, event):
         sleep(0.5)
         self.pedal = 0
+        self.update_vu(0)
 
-        val, rem = divmod(self.pedal, 10)
-        img = self.VU[str((val * 10) + (rem // 5 * 5))]
-        self.vu_lbl.configure(image=img)
-        self.vu_lbl.image = img
-        self.pad_lbl.configure(text='{:>4}%'.format(self.pedal))
+    def map_vu_meter(self, speed):
+        large_div, small_div = 10, 5
+        val, rem = divmod(speed, large_div)
+        return (val * large_div) + (rem // small_div * small_div)
+
+    def update_vu(self, speed):
+        vu_val = self.map_vu_meter(speed)
+        vu_img = self.VU[str(vu_val)]
+        self.vu_lbl.configure(image=vu_img)
+        self.vu_lbl.image = vu_img
+        self.pad_lbl.configure(text='{:>4}%'.format(speed))
+
+    def map_bett_percent(self, percent):
+        val, rem = divmod(percent, 25)
+        offset = 1 if rem != 0 else 0
+        return (val + offset) * 25
+
+    def update_batt(self, percent):
+        batt_val = self.map_bett_percent(percent)
+        batt_img = self.BATT[str(batt_val)]
+        self.battery_img.configure(image=batt_img)
+        self.battery_img.image = batt_img
+        self.battery_lbl.configure(text="{:>4}%".format(percent))
 
     def update_time(self):
-        _time = datetime.now().strftime("%d/%m/%Y\n%H:%M:%S")
-        self.time_lbl.configure(text=_time)
         self.count += 1
-        minutes, seconds = divmod(divmod(self.count, 3600)[1], 60)
-        _elapse_time = "{:02d}:{:02d}".format(int(minutes), int(seconds))
-        self.elapse_lbl.config(text=_elapse_time)
+        self.time_lbl.configure(text=get_datetime())
+        self.elapse_lbl.config(text=to_time_format(self.count))
         self.after(1000, self.update_time)
+
+    def _init_image_label(self, parent, image):
+        lbl = Label(parent, image=image, borderwidth=0)
+        lbl.image = image
+        return lbl
+
+    def _init_text_label(self, parent, text, font_size, fg='white'):
+        lbl = Label(parent, text=text, bg="#212121", fg=fg)
+        lbl.config(font=("fangsongti", font_size))
+        return lbl
 
     def initUI(self):
         self.master.title("Absolute positioning")
@@ -75,87 +105,56 @@ class Example(Frame):
         vu_labelframe = LabelFrame(self, bg="#212121", borderwidth=0)
         vu_labelframe.pack(side="left", anchor='w', fill='y')
 
-        img_logo = self.LOGO['edr']
-        edr_lbl = Label(vu_labelframe, image=img_logo, borderwidth=0)
-        edr_lbl.image = img_logo
-        edr_lbl.pack(side="top")
+        self._init_image_label(vu_labelframe, self.LOGO['edr']).pack(anchor="n")
 
-        _time = datetime.now().strftime("%d/%m/%Y\n%H:%M:%S")
-        self.time_lbl = Label(vu_labelframe, text=_time, bg="#212121", fg="white")
-        self.time_lbl.config(font=("fangsongti", 44))
+        self.time_lbl = self._init_text_label(vu_labelframe, get_datetime(), 45)
         self.time_lbl.pack(side='top')
 
-        km_lbl = Label(vu_labelframe, text="KM", bg="#212121", fg="white")
-        km_lbl.config(font=("Lucida Console", 45))
-        km_lbl.pack(side='right', anchor="se")
-        dist_lbl = Label(vu_labelframe, text="{:5.2f}".format(self.distance), bg="#212121", fg="white")
-        dist_lbl.config(font=("Lucida Console", 85))
-        dist_lbl.pack(side='bottom', anchor="s")
+        self.dist_lbl = self._init_text_label(vu_labelframe, "{:5.2f} KM".format(self.distance), 85)
+        self.dist_lbl.pack(side='bottom', anchor="s")
 
-        kw_lbl = Label(vu_labelframe, text="KW", bg="#212121", fg="white")
-        kw_lbl.config(font=("Lucida Console", 45))
-        kw_lbl.pack(side='right', anchor="se", pady=50)
-        pw_lbl = Label(vu_labelframe, text="{:04d}".format(self.power), bg="#212121", fg="white")
-        pw_lbl.config(font=("Lucida Console", 75))
-        pw_lbl.pack(side='bottom', anchor="s", pady=50)
+        self.pw_lbl = self._init_text_label(vu_labelframe, "{:04d} KW".format(self.power), 75)
+        self.pw_lbl.pack(side='bottom', anchor="s", pady=50)
 
         # =============================================================
 
         spd_labelframe = LabelFrame(self, borderwidth=0, bg="#212121")
         spd_labelframe.pack(side='left', fill='y')
 
-        img_vu = self.VU['0']
-        self.vu_lbl = Label(spd_labelframe, image=img_vu, borderwidth=0)
-        self.vu_lbl.image = img_vu
+        self.vu_lbl = self._init_image_label(spd_labelframe, self.VU['0'])
         self.vu_lbl.pack(anchor='n')
-
-        self.pad_lbl = Label(spd_labelframe, text="{:>4}%".format(0), bg="#212121", fg="white")
-        self.pad_lbl.config(font=("fangsongti", 85))
+        self.pad_lbl = self._init_text_label(spd_labelframe, "{:>4}%".format(0), 85)
         self.pad_lbl.pack(anchor='n')
 
-        spd_lbl = Label(spd_labelframe, text="{:6.2f}".format(0), bg="#212121", fg="#65dba8")
-        spd_lbl.config(font=("fangsongti", 200))
-        spd_lbl.pack(side='top')
-        unit_lbl = Label(spd_labelframe, text="Km/hr", bg="#212121", fg="#65dba8")
-        unit_lbl.config(font=("fangsongti", 64))
-        unit_lbl.pack(side='top')
+        self.spd_lbl = self._init_text_label(spd_labelframe, "{:4.2f}".format(0), 200, fg="#65dba8")
+        self.spd_lbl.pack(side='top')
+        self._init_text_label(spd_labelframe, "Km/hr", 64, fg="#65dba8").pack(side='top')
 
         sensor_labelframe = LabelFrame(spd_labelframe, borderwidth=0, bg="#212121")
         sensor_labelframe.pack(side='bottom', padx=150)
 
-        img_hall = self.HALL['0']
-        hall_lbl = Label(sensor_labelframe, image=img_hall, borderwidth=0)
-        hall_lbl.image = img_hall
-        hall_lbl.pack(side="left")
+        self.hall_lbl = self._init_image_label(sensor_labelframe, self.HALL['0'])
+        self.hall_lbl.pack(side="left")
 
-        img_temp = self.TEMP[str(self.temp)]
-        temp_lbl = Label(sensor_labelframe, image=img_temp, borderwidth=0)
-        temp_lbl.image = img_temp
-        temp_lbl.pack(side="left")
+        self.temp_lbl = self._init_image_label(sensor_labelframe, self.TEMP[str('0')])
+        self.temp_lbl.pack(side="left")
 
         # =============================================================
 
         right_labelframe = LabelFrame(self, bg="#212121", borderwidth=0)
         right_labelframe.pack(side='left', fill='y')
 
-        batt_width, batt_height = 150, 100
         batt_labelframe = LabelFrame(right_labelframe, bg="#212121", borderwidth=0)
-        batt_labelframe.pack(side="top", anchor='n', padx=batt_width // 10, pady=batt_height // 5)
+        batt_labelframe.pack(side="top", anchor='n')
 
-        img_batt = self.BATT[str(self.batt_percentage // 10 *10)]
-        battery_lbl = Label(batt_labelframe, image=img_batt, borderwidth=0)
-        battery_lbl.image = img_batt
-        battery_lbl.pack(side='right', anchor='e')
-        batt_lbl = Label(batt_labelframe, text="{:>4}%".format(self.batt_percentage), bg="#212121", fg="white")
-        batt_lbl.config(font=("fangsongti", 75))
-        batt_lbl.pack(side='top', anchor='e', padx=(0, batt_width // 10))
+        batt_per = self.batt_percentage // 25 * 25
+        self.battery_img = self._init_image_label(batt_labelframe, self.BATT[str(batt_per)])
+        self.battery_img.pack(side='right', anchor='e')
 
-        hours, remaining = divmod(self.count, 3600)
-        minutes, seconds = divmod(remaining, 60)
-        _elapse_time = "{:02d}:{:02d}".format(int(minutes), int(seconds))
+        self.battery_lbl = self._init_text_label(batt_labelframe, "{:>4}%".format(self.batt_percentage), 65)
+        self.battery_lbl.pack(side='top', anchor='e', padx=(0, 50))
 
-        self.elapse_lbl = Label(right_labelframe, text=_elapse_time, bg="#212121", fg="white")
-        self.elapse_lbl.config(font=("Lucida Console", 80))
+        self.elapse_lbl = self._init_text_label(right_labelframe, to_time_format(self.count), 80)
         self.elapse_lbl.pack(side='bottom')
 
 
@@ -168,4 +167,3 @@ if __name__ == '__main__':
 
     app = Example(root, width, height)
     root.mainloop()
-
